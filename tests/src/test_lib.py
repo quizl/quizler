e1d7312499b96a93d5ba4e40348aa7b6d7c68a36
Pavel Karateev <karateev.pavel@ya.ru>
2017-07-03 11:39:13 +0300

    Fixes tests for api_call

--- tests/src/test_lib.py
+++ tests/src/test_lib.py
@@ -5,14 +5,18 @@ from src.lib import api_call
 
 
 class TestApiCall(unittest.TestCase):
-    def test_unknown_endpoint(self):
-        with mock.patch('requests.get') as mock_get:
-            response = mock_get.return_value
-            response.status_code = 404
-            with self.assertRaises(ValueError):
-                api_call('unknown_end_point')
+    @mock.patch('requests.get')
+    def test_unknown_endpoint(self, mock_get):
+        mock_get.return_value.status_code = 404
+        with self.assertRaises(ValueError):
+            api_call('unknown_end_point')
 
-    def test_correct_url_was_called(self):
-        with mock.patch('requests.get') as mock_get:
-            api_call('end_point')
-            mock_get.assert_called_once_with()
+    @mock.patch('requests.get')
+    @mock.patch('src.lib.CLIENT_ID', 'user_id_123')
+    def test_correct_url_was_called(self, mock_get):
+        mock_get.return_value.status_code = 200
+        api_call('end_point')
+        mock_get.assert_called_once_with(
+            'https://api.quizlet.com/2.0/users/pavel_karateev/end_point',
+            {'client_id': 'user_id_123'}
+        )
